e5bf3ef1e4505f30a19431b7d8fc3b3fae85be04
yoyoman21 <67953086+yoyoman21@users.noreply.github.com>
2020-07-23 12:35:44 +0530

    Add files via upload

---
+++ Day 1 Assignments/demo2.js
@@ -0,0 +1,22 @@
+document.write("1.console.log():-Mainly used to log(print) the output to the console.Inside log(),it can be a string,array,object,boolean,etc");
+document.write("<br/>");
+document.write("e.g:-open console to see ouput");
+document.write("<br/>");
+console.time('Time taken');
+    console.log("I study in keystone");
+document.write("2.console.warn():-It is used to log warning messaages to the console.By default the warning msg will be in yellow colou.");
+document.write("<br/>");
+ document.write("e.g:-open console to see ouput");
+ document.write("<br/>");
+    console.warn('warning!');   
+ document.write("<br/>3.console.error():-returns the error in console log");
+ document.write("<br/>e.g:-open console to see ouput");
+    console.error("error!");
+  document.write("<br/>4.console.table():-This method allows us to generate a table inside a console.The inpu mustbe a array or object");
+  document.write("<br/>e.g:-open console to see ouput");
+    console.table({'a':1,'b':2});
+    document.write("<br/>5.console.timeEnd():-It returns the time")
+     document.write("<br/>e.g:-open console to see ouput");
+     console.timeEnd('Time taken');
+
+
